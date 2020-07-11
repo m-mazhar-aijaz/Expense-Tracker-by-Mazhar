a0677f1026703b33547663c2b26161304dd14892
Muhammad Mazhar Aijaz <66208725+m-mazhar-aijaz@users.noreply.github.com>
2020-07-11 18:52:30 +0500

    Update
    Disable dispatch function

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -18,7 +18,7 @@ export const GlobalContext = createContext(initialState);
 
 // Create a Provider for the Global Context
 export const GlobalProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(AppReducer, initialState);
+    //const [state, dispatch] = useReducer(AppReducer, initialState);
  
     return (<GlobalContext.Provider value={{
                 transactions: state.transactions
